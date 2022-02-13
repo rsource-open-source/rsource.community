482724a6bec03522f29d2df674490b0e47e5be5e
insyri <60794909+insyri@users.noreply.github.com>
2022-02-12 19:18:53 -0500

    shh

--- pages/[page].tsx
+++ pages/[page].tsx
@@ -1,5 +1,5 @@
 // import teleporter from "../utils/functions/teleporter";
-import Metadata from "../utils/components/metadata";
+// import Metadata from "../utils/components/metadata";
 import Head from "next/head";
 import { colors } from "../utils/constants";
 
@@ -12,7 +12,7 @@ export default function page(): JSX.Element {
           data-react-helmet="true"
           name="theme-color"
         ></meta>
-        <Metadata />
+        {/* <Metadata /> */}
       </Head>
     </>
   );
