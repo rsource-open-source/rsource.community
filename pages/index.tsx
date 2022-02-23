924be18da5ae9181c327512c2f0a974aecebfad1
insyri <60794909+insyri@users.noreply.github.com>
2022-02-22 19:22:37 -0500

    installed tailwind and cleared css

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ for (const [k, v] of Object.entries(routes))
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>rsource community</title>
         <meta
@@ -93,7 +93,7 @@ const Home: NextPage = () => {
         </div>
         {/* <canvas id="bg"></canvas> */}
       </main>
-    </div>
+    </>
   );
 };
 
