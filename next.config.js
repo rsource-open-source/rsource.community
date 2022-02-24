8b1915f34c48d816c705f332e64d3e8a83a9e182
insyri <60794909+insyri@users.noreply.github.com>
2022-02-23 20:20:16 -0500

    i tried npm routes :/

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
-const routes = {
-  records: "https://github.com/rsource-open-source/rsource-records",
-  android: "https://github.com/rsource-open-source/rsource-android",
-  redirect: "https://github.com/rsource-open-source/rsource-redirect",
-  api: "https://github.com/rsource-open-source/rsource-api",
-  "api-docs": "https://postman.com/rsource-open-source/workspace/rsource-api",
-  twitch: "https://github.com/rsource-open-source/rsource-twitch",
-  pylon: "https://github.com/rsource-open-source/server-pylon",
-  website: "https://github.com/rsource-open-source/website",
-  ahks: "https://github.com/rsource-open-source/ahks",
-  extractor: "https://github.com/rsource-open-source/extractor",
-  gist: "https://github.com/rsource-open-source/gists",
-  proposal: "https://github.com/rsource-open-source/strafes-net-api-proposal",
-  projects: "https://github.com/orgs/rsource-open-source/projects/3",
-  discord: "https://discord.gg/2uFfQ3WYNX",
-  organization: "https://github.com/rsource-open-source",
-  roblox: "https://www.roblox.com/groups/10125873/rsource-community",
-};
+// const rsource_routes = require("rsource-routes");
+// console.log(rsource_routes);
 
 let nextRoutes = [];
 
