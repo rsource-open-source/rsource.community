59a5496f2d46413c45b7642c2ebc53a3f20e5a67
insyri <60794909+insyri@users.noreply.github.com>
2022-03-27 01:33:49 -0400

    start on #14

---
+++ pages/repo/[[...params]].tsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import { GetServerSideProps } from "next/types";
+import redirectRoutes from "../../utils/routes";
+import { readFileSync } from "fs";
+import { Redirect } from "next/dist/lib/load-custom-routes";
+const query = readFileSync("./request.gql", "utf8");
+
+function GetRepoSpecifics(redirects: Redirect[]): JSX.Element {
+  const router = useRouter();
+  const params = router.query.params;
+  if (!params) return <>undefined??? wtf!!!</>;
+
+  // check if params[0] is a valid redirect.
+  // if (Array.isArray(params)) return <>array??? wtf!!!</>;
+  // if (params.match(/.+\/.+:.+/g) === null)
+  // return <>bad param syntax??? wtf!!!</>;
+  return <>{params}</>;
+}
+
+export const getServerSideProps: GetServerSideProps<Redirect[]> = async () => {
+  // const res = await fetch("");
+  // const data = await res.json();
+  let props = await redirectRoutes(process.env.GITHUB_TOKEN!, query, true);
+
+  return {
+    props,
+  };
+};
+
+export default GetRepoSpecifics;
